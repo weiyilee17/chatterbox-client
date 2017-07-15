caa6fd43f2aaf7d949eb15c380e2bd64182a9148
Hack Reactor Student <student@hackreactor.com>
2017-07-14 17:26:33 -0700

    Implemented send and fetch methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,63 @@
-// YOUR CODE HERE:
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+var app = {
+  server: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+  init: function () {
+  
+  },
+  send: function (message) {
+    $.ajax({
+      //This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function (url) {
+    $.ajax({
+      //This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      //data: JSON.stringify(message),
+      //contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });    
+  },
+  clearMessages: function () {
+    
+  },
+};
